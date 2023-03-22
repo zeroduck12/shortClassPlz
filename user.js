6703afb9c16120e2583370fa0c181174324bbc26
csj20222 <csj20222@gmail.com>
2023-03-22 17:35:20 +0900

    Update user.js
    코드 전부 변경

--- user.js
+++ user.js
@@ -14,57 +14,47 @@
 	 //휴대폰 번호	: 빈값체크 ""
 	 
 	 
-	 //아이디 체크
-	 if($("#userid").val()==""){
-		 alert("아이디 입력하라니깐?");
-		$("#userid").focus();
+	 let frm = document.joinForm;
+	 let userid = frm.userid;
+	 let userpw = frm.userpw;
+	 let userpw_re = frm.userpw_re;
+	 let username = frm.username;
+	 let userphone = frm.userphone;
+	 
+	 if(userid.value ==''){
+		 alert("아이디를 입력해 주세요");
+		 userid.focus();
 		 return false;
 	 }
-	 
-	 //아이디 5자리 이상
- 	 if($("#userid").val().length<5){
-		alert("아이디 짧어~5글자 이상입력해");
-	 	$("#userid").focus();
-	 	return false;
-	 }
-	 	//아이디 16자리 미만
- 	 if($("#userid").val().length>16){
-		alert("아이디 길어~16글자 미만입력해");
-	 	$("#userid").focus();
-	 	return false;
-	 }
-	 
-	 
-	 //비밀 번호 	: 빈값체크 "", 8자리 이상, 비밀번호 = 비밀번호 확인이 같은지 확인
-	 if($("#userpw").val()==""){
-		 alert("비번 입력하라니깐?");
-		 $("#userpw").focus();			
+	 if(userid.value.length < 5 || userid.value.length > 16){
+		 alert("아이디를 5자 이상 16자 미만으로 입력해주세요");
+		 userid.focus();
 		 return false;
 	 }
-	 
-	  if($("#userpw").val().length<8){
-		alert("비번 짧어~ 8자리 이상입력해");
-	 	$("#userpw").focus();			
-	 	return false;
-	 }
-
-	if($("#userpw").val() != $("#userpw_re").val()){
-		alert("비번2개가 서로 다르자나");
-	 	$("#userpw").focus();
-	 	return false;
-	 }
-	 //이름 확인
- 	 if($("#username").val()==""){
-		 alert("이름 입력하라니깐?");
-		 $("#username").focus();		
+	 if(userpw.value == ""){
+		 alert("비밀번호를 입력해주세요");
+		 userpw.focus();
 		 return false;
 	 }
-	 //휴대폰 번호
- 	 if($("#userphone").val()==""){
-		 alert("휴대폰 번호 입력하라니깐?");
-		 $("#userphone").focus();
+	 if(userpw.value.length < 8){
+		 alert("비밀번호를 8자 이상 입력해주세요");
+		 userpw.focus();
 		 return false;
 	 }
-	 
-	 $("#joinForm").submit();
+	 if(userpw.value != userpw_re.value){
+		 alert("비밀번호가 일치하지 않습니다.");
+		 userpw.focus();
+		 return false;
+	 }
+ 	 if(username.value == ""){
+		 alert("이름을 입력해주세요");
+		 username.focus();
+		 return false;
+	 }
+ 	 if(userphone.value == ""){
+		 alert("전화번호를 입력해주세요");
+		 userphone.focus();
+		 return false;
+	 }
+	frm.submit();
  }
